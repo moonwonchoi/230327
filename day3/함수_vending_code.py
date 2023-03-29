2af285915c67943e0d492f65b708c5d552cfa2c4
moonwonchoi <classshare1234@gmail.com>
2023-03-29 14:48:50 +0900

    sample

---
+++ day3/함수_vending_code.py
@@ -0,0 +1,18 @@
+def ShowMenu():   
+     pass
+def BuyItem(num):    
+    pass 
+def ShowSellList():
+    pass
+    
+def CheckPassword():
+    pass
+ 
+menu = ("콜라", "사이다", "생수", "커피")
+price = (1200, 1000, 700, 1500)
+money = 0
+money = int( input("돈을 투입하세요 : ") )
+selllist = {}
+
+while True:
+    pass
